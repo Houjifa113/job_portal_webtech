5c2a362fbee379fe8bfce99684deeaef680dd721
Nayeemul Islam Nayeem <118336797+Krak3n909@users.noreply.github.com>
2025-08-19 23:59:55 +0600

    resume-complete

--- 23-51350-1/Resume_Upload/script.js
+++ 23-51350-1/Resume_Upload/script.js
@@ -1,8 +1,18 @@
 function resumeUpload()
 {
     let fileInput = document.getElementById('resumeFile');
+    let fileError= document.getElementById('resumeFileError');
     if (fileInput.files.length === 0) {
-        alert("Please select a file to upload.");
+        fileError.innerHTML = "Please select a file to upload!";
+        fileError.style.color = "red";
+        return false;
+    }
+    let fileName = document.getElementById('resumeFile').value;
+    let fileExtension = fileName.split('.');
+    fileExtension = fileExtension[fileExtension.length - 1].toLowerCase();
+    if (fileExtension !== 'pdf' && fileExtension !== 'docx') {
+        fileError.innerHTML = "Please select PDF or DOC!";
+        fileError.style.color = "red";
         return false;
     }
     return true;
