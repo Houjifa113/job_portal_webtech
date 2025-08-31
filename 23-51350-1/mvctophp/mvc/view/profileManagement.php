8ad2a20ee58b35a359a91a2c43d5b4c3a3f8bab8
Nayeemul Islam Nayeem <118336797+Krak3n909@users.noreply.github.com>
2025-08-31 23:33:16 +0600

    added session and cookie validation and small fixes

---
+++ 23-51350-1/mvctophp/mvc/view/profileManagement.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Profile Manager</title>
+    <link rel="stylesheet" href="../asset/style.css">
+</head>
+<body>
+    <a href="./home.php"><input type="button" value="Home" id="home" class="profile-button" style="position: absolute;left: 2%;top: 2%;width: 8%;"></a>
+    <table class="standardTable">
+    <tr>
+    <td style="padding: 3%;"><a href="./viewProfile.php"><input type="button" value="View Profile" id="viewProfile" class="profile-button"></a></td>
+    </tr>
+    <tr>
+    <td style="padding: 3%;"><a href="./editProfile.php"><input type="button" value="Edit Profile" id="editProfile" class="profile-button"></a></td>
+    </tr>
+    <tr>
+    <td style="padding: 3%;"><a href="./changePicture.php"><input type="button" value="Change Picture" id="changePicture" class="profile-button"></a></td>
+    </tr>
+    <tr>
+    <td style="padding: 3%;"><a href="./updatePassword.php"><input type="button" value="Update Password" id="updatePassword" class="profile-button"></a></td>
+    </tr>
+    </table>
+    <script src="../asset/script.js"></script>
+</body>
+</html>
